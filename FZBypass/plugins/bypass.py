88018f001945055b823148f7212d253621e53b9f
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2023-12-01 01:17:24 +0530

    Update bypass.py

--- FZBypass/plugins/bypass.py
+++ FZBypass/plugins/bypass.py
@@ -24,17 +24,16 @@ BypassFilter = create(auto_bypass)
 
 @Bypass.on_message(command('start'))
 async def start_msg(client, message):
-    await message.reply(f'''<b><i>FZ Bypass Bot!</i></b>
-    
-    <i>A Powerful Elegant Multi Threaded Bot written in Python... which can Bypass Various Shortener Links, Scrape links, and More ... </i>
-    
+    await message.reply(f'''Hi I am <b><i>RS Link Bypass Bot!</i></b>
+    <i>I can bypass Various Shortener Links, Scrape links, and More ... </i>    
     <i><b>Bot Started {convert_time(time() - BOT_START)} ago...</b></i>
 
-🛃 <b>Use Me Here :</b> @CyberPunkGrp <i>(Bypass Topic)</i>''',
+🏴‍☠️ <b>Use Me in Bypass World</b> 🏴‍☠️''',
         quote=True,
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton('🎓 Dev', url='https://t.me/SilentDemonSD'), InlineKeyboardButton('🔍 Deploy Own', url="https://github.com/SilentDemonSD/FZBypassBot")]
-            ])
+    [InlineKeyboardButton('🏴‍☠️ Bypass World 🏴‍☠️', url='https://t.me/+54GHlNstXvMzMWRl')],
+    [InlineKeyboardButton('📣 Our Channel 📣', url='https://telegram.me/rs_bro'), InlineKeyboardButton('🤵🏻 Contact Admin 🤵🏻', url="https://telegram.me/rs_m_bot")]
+])
     )
 
 
@@ -156,15 +155,14 @@ async def inline_query(client, query):
         answers.append(InlineQueryResultArticle(
                 title="♻️ Bypass Usage: In Line",
                 input_message_content=InputTextMessageContent(
-                    '''<b><i>FZ Bypass Bot!</i></b>
-    
-    <i>A Powerful Elegant Multi Threaded Bot written in Python... which can Bypass Various Shortener Links, Scrape links, and More ... </i>
+                    '''Hi I am <b><i>RS Link Bypass Bot!</i></b>    
+    <i>I can bypass Various Shortener Links, Scrape links, and More ... </i>  
     
 🎛 <b>Inline Use :</b> !bp [Single Link]''',
                 ),
                 description="Bypass via !bp [link]",
                 reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("FZ Channel", url="https://t.me/FXTorrentz"),
+                        [InlineKeyboardButton("Our Channel", url="https://telegram.me/rs_bro"),
                         InlineKeyboardButton('Try Bypass', switch_inline_query_current_chat="!bp ")]
                 ])
             ))
