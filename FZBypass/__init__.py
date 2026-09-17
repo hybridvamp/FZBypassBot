b1673895aa922b52f706e5fa5cbb105e3b43c11b
rjriajul <rj@rj1.dev>
2026-09-17 12:29:04 +0600

    refactor: move plugins to FZBypass/handler
    Point the plugin root at the new location.

--- FZBypass/__init__.py
+++ FZBypass/__init__.py
@@ -56,6 +56,6 @@ Bypass = Client(
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="FZBypass/bypass/plugins"),
+    plugins=dict(root="FZBypass/handler"),
     parse_mode=ParseMode.HTML,
 )
